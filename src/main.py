41bd85cc414acdfed092b73993c5c0eeb913793a
Kanav Goyal <kanavgoyal898@gmail.com>
2025-04-22 18:32:30 +0530

    edit hyperparameters

--- src/main.py
+++ src/main.py
@@ -12,19 +12,16 @@ random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
 
-transform = torchvision.transforms.Compose([
+transforms = torchvision.transforms.Compose([
     torchvision.transforms.Resize((IMG_SIZE, IMG_SIZE)),
     torchvision.transforms.CenterCrop(IMG_SIZE),
     torchvision.transforms.ToTensor(),
 ])
 
-dataset_train = torchvision.datasets.Flowers102(root='../data/', split='train', download=True, transform=transform)
-dataset_val = torchvision.datasets.Flowers102(root='../data/', split='val', download=True, transform=transform)
-dataset_train_val = torch.utils.data.ConcatDataset([dataset_train, dataset_val])
+dataset_train = torchvision.datasets.OxfordIIITPet(root='../data/', split='trainval', download=True, transform=transforms)
+dataset_test = torchvision.datasets.OxfordIIITPet(root='../data/', split='test', download=True, transform=transforms)
 
-dataset_test = torchvision.datasets.Flowers102(root='../data/', split='test', download=True, transform=transform)
-
-train_valid_loader = torch.utils.data.DataLoader(dataset_train_val, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+train_valid_loader = torch.utils.data.DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
 test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
 
 model = UNetLikeLite()
